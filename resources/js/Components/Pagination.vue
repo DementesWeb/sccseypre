0128c756491fadde25541791d8d206e88d0b2572
juanca2918 <jacal2211@gmail.com>
2022-05-02 18:02:47 -0500

    Se agrega la vista Consulta cedula con su componente pagination

---
+++ resources/js/Components/Pagination.vue
@@ -0,0 +1,56 @@
+<template>
+    <!-- This example requires Tailwind CSS v2.0+ -->
+    <div v-if="pagination.links.length > 3" class="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
+        <div class="flex-1 flex justify-between sm:hidden">
+            <a href="#" class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"> Anterior </a>
+            <a href="#" class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"> Siguiente </a>
+        </div>
+        <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+            <div>
+            <p class="text-sm text-gray-700">
+                Mostrando
+                <span class="font-medium">{{ pagination.from }}</span>
+                hasta
+                <span class="font-medium">{{ pagination.to }}</span>
+                de
+                <span class="font-medium">{{ pagination.total }}</span>
+                resultados
+            </p>
+            </div>
+            <div>
+                <nav class="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
+
+                    <template v-for="(link, key) in pagination.links">
+                        <div :key="key" v-if="link.url == null" class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium" v-html="link.label">
+                        </div>
+                            <Link :key="'link-' + key"
+                                v-else
+                                :href="link.url"
+                                class="relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+                                :class="link.active ? 'z-10 bg-indigo-50 border-indigo-500 text-indigo-600' : 'bg-white border-gray-300 text-gray-500 hover:bg-gray-50'"
+                                v-html="link.label"
+                            />
+                    </template>
+
+                    <!-- <pre>{{ pagination }}</pre> -->
+
+                    <!-- Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" -->
+                    <!-- <a href="#" class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"> 2 </a> -->
+
+                </nav>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { Link } from '@inertiajs/inertia-vue3'
+export default {
+    components: {
+        Link
+    },
+    props:{
+        pagination:Object
+    }
+}
+</script>
