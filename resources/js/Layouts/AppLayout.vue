a0a8fdfc89f29f82febaa1c001c40f60f4ded2ff
esvirenson <st-rodriguez@hotmail.com>
2022-06-30 14:28:01 -0500

    Se agreaga Módulo BusquedaMultiple

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -31,6 +31,10 @@
                                     Buscar Cédula
                                 </jet-nav-link>
 
+                                <jet-nav-link :href="route('BusquedaMultiple')" :active="route().current('BusquedaMultiple')">
+                                    Búsqueda Múltiple
+                                </jet-nav-link>
+
                                 <div class="hidden sm:flex sm:items-center sm:ml-6">
                                     <jet-dropdown align="right" width="60" v-if="$page.props.jetstream.hasTeamFeatures">
                                     <template #trigger v-if="$page.props.user.current_team_id == 1">
